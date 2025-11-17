f69eedcba22e7944a432ff79357e2895ba90b51d
Avsar Koktas <avsarkoktas@icloud.com>
2025-11-17 15:30:33 +0100

    form zn film namen recht gezet

--- Film.Kom/obj/Debug/net8.0-windows/Film.Kom.AssemblyInfo.cs
+++ Film.Kom/obj/Debug/net8.0-windows/Film.Kom.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Film.Kom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4119d9f439fb65a298638ba88c39f4f2ef47b38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ea21243296c36e6351ae50b5f35a00ea7cf039")]
 [assembly: System.Reflection.AssemblyProductAttribute("Film.Kom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Film.Kom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

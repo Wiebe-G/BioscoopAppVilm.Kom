2949049126bb66ecad7e903c98f7caafa631ef24
Wiebe Gouma <goumawiebe1234@proton.me>
2025-11-26 10:54:34 +0100

    Zoekfunctie op frmFilmInfo gebruikt nu ook de api niet meer

--- Film.Kom/frmFilmInfo.cs
+++ Film.Kom/frmFilmInfo.cs
@@ -42,37 +42,18 @@ namespace Film.Kom
 
         internal async Task GetMovieInfo(string FilmName)
         {
-            // fetch met omdbapi info over de film
-            // en display de info
-            string APIkey = passwords.APIKey;
-            string BaseURL = "https://www.omdbapi.com";
-
-            using (var client = new HttpClient())
+            Searchfunction SearchForFilms = new();
+            var MovieData = await SearchForFilms.SearchFunction(FilmName.Trim().ToLower());
+            if (MovieData == null)
             {
-                client.BaseAddress = new Uri(BaseURL);
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-
-                var QueryString = $"t={Uri.EscapeDataString(FilmName)}&plot=full&&apikey={APIkey}";
-                HttpResponseMessage response = await client.GetAsync($"?{QueryString}");
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    MessageBox.Show($"Oh oh, er ging iets niet goed. Statuscode: {response.StatusCode}. Reden: {response.ReasonPhrase}");
-                    return;
-                }
-                string JSONResponse = await response.Content.ReadAsStringAsync();
-                var MovieData = JsonSerializer.Deserialize<FilmInfo>(JSONResponse);
-                // vraagteken betekent dat de variabele null mag zijn
-                if (MovieData?.Response != "True")
-                {
-                    MessageBox.Show("Film niet gevonden");
-                    return;
-                }
-                if (!string.IsNullOrWhiteSpace(MovieData.Poster) && MovieData.Poster != "N/A")
-                {
-                    DisplayData(MovieData.Poster, MovieData.Title, MovieData.Plot, MovieData.Rated);
-                }
+                MessageBox.Show($"Film is niet gevonden. probeer het opnieuw");
+                return;
             }
+            if (!string.IsNullOrWhiteSpace(MovieData.Poster) && MovieData.Poster != "N/A")
+            {
+                DisplayData(MovieData.Poster, MovieData.Title, MovieData.Plot, MovieData.Rated);
+            }
+
         }
         private void DisplayData(string ImageLocation, string Title, string Plot, string Rating)
         {

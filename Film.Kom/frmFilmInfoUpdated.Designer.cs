0c9f1d17f5e93cf7ac70f828e76cb54e3a791262
Avsar Koktas <avsarkoktas@icloud.com>
2025-12-16 13:03:43 +0100

    Film Info logo toegevoegd

--- Film.Kom/frmFilmInfoUpdated.Designer.cs
+++ Film.Kom/frmFilmInfoUpdated.Designer.cs
@@ -38,17 +38,22 @@
             pnlMaininfo = new TableLayoutPanel();
             picPoster = new PictureBox();
             pnlSecondinfo = new TableLayoutPanel();
-            lblPlot = new Label();
-            lblRating = new Label();
             pnlTabReservering = new TableLayoutPanel();
             btnReservering = new Button();
+            lblRating = new Label();
+            lblPlot = new Label();
+            picLogo = new PictureBox();
+            btnLogin = new Button();
             pnlMain.SuspendLayout();
             pnlSecond.SuspendLayout();
             tableLayoutPanel1.SuspendLayout();
+            pnlLogo.SuspendLayout();
+            pnlButtons.SuspendLayout();
             pnlMaininfo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)picPoster).BeginInit();
             pnlSecondinfo.SuspendLayout();
             pnlTabReservering.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)picLogo).BeginInit();
             SuspendLayout();
             // 
             // pnlMain
@@ -118,26 +123,29 @@
             pnlLogo.ColumnCount = 2;
             pnlLogo.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             pnlLogo.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            pnlLogo.Controls.Add(picLogo, 0, 0);
             pnlLogo.Dock = DockStyle.Fill;
             pnlLogo.Location = new Point(3, 3);
             pnlLogo.Name = "pnlLogo";
-            pnlLogo.RowCount = 2;
-            pnlLogo.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            pnlLogo.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            pnlLogo.RowCount = 1;
+            pnlLogo.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            pnlLogo.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
             pnlLogo.Size = new Size(373, 109);
             pnlLogo.TabIndex = 3;
             // 
             // pnlButtons
             // 
-            pnlButtons.ColumnCount = 2;
+            pnlButtons.ColumnCount = 3;
             pnlButtons.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             pnlButtons.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            pnlButtons.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 126F));
+            pnlButtons.Controls.Add(btnLogin, 2, 0);
             pnlButtons.Dock = DockStyle.Fill;
             pnlButtons.Location = new Point(1520, 3);
             pnlButtons.Name = "pnlButtons";
-            pnlButtons.RowCount = 2;
-            pnlButtons.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            pnlButtons.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            pnlButtons.RowCount = 1;
+            pnlButtons.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            pnlButtons.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
             pnlButtons.Size = new Size(375, 109);
             pnlButtons.TabIndex = 4;
             // 
@@ -186,37 +194,11 @@
             pnlSecondinfo.Size = new Size(1198, 908);
             pnlSecondinfo.TabIndex = 1;
             // 
-            // lblPlot
-            // 
-            lblPlot.AutoSize = true;
-            lblPlot.Dock = DockStyle.Fill;
-            lblPlot.Font = new Font("Georgia", 12F);
-            lblPlot.ForeColor = SystemColors.Control;
-            lblPlot.Location = new Point(3, 0);
-            lblPlot.Name = "lblPlot";
-            lblPlot.Size = new Size(1192, 302);
-            lblPlot.TabIndex = 1;
-            lblPlot.Text = "Plot";
-            lblPlot.TextAlign = ContentAlignment.MiddleCenter;
-            // 
-            // lblRating
-            // 
-            lblRating.AutoSize = true;
-            lblRating.Dock = DockStyle.Fill;
-            lblRating.Font = new Font("Georgia", 12F);
-            lblRating.ForeColor = SystemColors.Control;
-            lblRating.Location = new Point(3, 302);
-            lblRating.Name = "lblRating";
-            lblRating.Size = new Size(1192, 302);
-            lblRating.TabIndex = 2;
-            lblRating.Text = "Rating";
-            lblRating.TextAlign = ContentAlignment.MiddleCenter;
-            // 
             // pnlTabReservering
             // 
             pnlTabReservering.ColumnCount = 3;
-            pnlTabReservering.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
-            pnlTabReservering.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            pnlTabReservering.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.55705F));
+            pnlTabReservering.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.05369F));
             pnlTabReservering.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
             pnlTabReservering.Controls.Add(btnReservering, 1, 1);
             pnlTabReservering.Dock = DockStyle.Fill;
@@ -224,8 +206,8 @@
             pnlTabReservering.Margin = new Padding(3, 2, 3, 2);
             pnlTabReservering.Name = "pnlTabReservering";
             pnlTabReservering.RowCount = 2;
-            pnlTabReservering.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            pnlTabReservering.RowStyles.Add(new RowStyle(SizeType.Percent, 50.0000076F));
+            pnlTabReservering.RowStyles.Add(new RowStyle(SizeType.Percent, 73.3333359F));
+            pnlTabReservering.RowStyles.Add(new RowStyle(SizeType.Percent, 26.666666F));
             pnlTabReservering.RowStyles.Add(new RowStyle(SizeType.Absolute, 15F));
             pnlTabReservering.Size = new Size(1192, 300);
             pnlTabReservering.TabIndex = 5;
@@ -234,14 +216,65 @@
             // 
             btnReservering.BackColor = Color.Maroon;
             btnReservering.Dock = DockStyle.Fill;
+            btnReservering.Font = new Font("Georgia", 14.25F, FontStyle.Bold);
             btnReservering.ForeColor = SystemColors.Control;
-            btnReservering.Location = new Point(400, 151);
+            btnReservering.Location = new Point(403, 222);
             btnReservering.Margin = new Padding(3, 2, 3, 2);
             btnReservering.Name = "btnReservering";
-            btnReservering.Size = new Size(391, 147);
+            btnReservering.Size = new Size(388, 76);
             btnReservering.TabIndex = 3;
             btnReservering.Text = "Reserveer";
             btnReservering.UseVisualStyleBackColor = false;
+            // 
+            // lblRating
+            // 
+            lblRating.AutoSize = true;
+            lblRating.Dock = DockStyle.Fill;
+            lblRating.Font = new Font("Georgia", 14.25F, FontStyle.Bold);
+            lblRating.ForeColor = SystemColors.Control;
+            lblRating.Location = new Point(3, 302);
+            lblRating.Name = "lblRating";
+            lblRating.Size = new Size(1192, 302);
+            lblRating.TabIndex = 2;
+            lblRating.Text = "Rating";
+            lblRating.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblPlot
+            // 
+            lblPlot.AutoSize = true;
+            lblPlot.Dock = DockStyle.Fill;
+            lblPlot.Font = new Font("Georgia", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPlot.ForeColor = SystemColors.Control;
+            lblPlot.Location = new Point(3, 0);
+            lblPlot.Name = "lblPlot";
+            lblPlot.Size = new Size(1192, 302);
+            lblPlot.TabIndex = 1;
+            lblPlot.Text = "Plot";
+            lblPlot.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // picLogo
+            // 
+            picLogo.Dock = DockStyle.Fill;
+            picLogo.Image = Properties.Resources.Logo_van_Rick_oftewel_ChatGPT;
+            picLogo.Location = new Point(3, 2);
+            picLogo.Margin = new Padding(3, 2, 3, 2);
+            picLogo.Name = "picLogo";
+            picLogo.Size = new Size(180, 105);
+            picLogo.SizeMode = PictureBoxSizeMode.StretchImage;
+            picLogo.TabIndex = 1;
+            picLogo.TabStop = false;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.Maroon;
+            btnLogin.Dock = DockStyle.Fill;
+            btnLogin.Location = new Point(251, 2);
+            btnLogin.Margin = new Padding(3, 2, 3, 2);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(121, 105);
+            btnLogin.TabIndex = 2;
+            btnLogin.Text = "Log in";
+            btnLogin.UseVisualStyleBackColor = false;
             // 
             // frmFilmInfoUpdated
             // 
@@ -256,11 +289,14 @@
             pnlSecond.ResumeLayout(false);
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
+            pnlLogo.ResumeLayout(false);
+            pnlButtons.ResumeLayout(false);
             pnlMaininfo.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)picPoster).EndInit();
             pnlSecondinfo.ResumeLayout(false);
             pnlSecondinfo.PerformLayout();
             pnlTabReservering.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)picLogo).EndInit();
             ResumeLayout(false);
         }
 
@@ -279,5 +315,7 @@
         private Label lblRating;
         private TableLayoutPanel pnlTabReservering;
         private Button btnReservering;
+        private PictureBox picLogo;
+        private Button btnLogin;
     }
 }

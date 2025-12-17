33b178c425f090cd50d1bcf1f4e972ed81331f4b
Avsar Koktas <avsarkoktas@icloud.com>
2025-12-17 09:26:15 +0100

    Code toegevoegd voor een slidehsow die de zeldfde posters uit de DB gebruikt als de films waar je op klikt

--- Film.Kom/frmMainMenu.cs
+++ Film.Kom/frmMainMenu.cs
@@ -2,13 +2,9 @@
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,48 +15,26 @@ namespace Film.Kom
         private readonly Passwords passwords = new();
         private readonly User _LoggedInUser;
         private IMongoCollection<FilmInfo> _Films;
+
+        // Slideshow
+        private List<string> _slideshowPosters = new();
+        private int _slideshowIndex = 0;
+        private Random _random = new Random();
+        private string _lastPoster = "";
+
         public frmMainMenu()
         {
             InitializeComponent();
             _LoggedInUser = new User();
             DisplayFiveRandomFilmsOnHomePage();
-        }
-
-        private void DisplayFiveRandomFilmsOnHomePage()
-        {
-            List<PictureBox> posters =
-            [
-                picFilm1, picFilm2, picFilm3, picFilm4, picFilm5
-            ];
-            List<Label> labels =
-            [
-                lblFilm1, lblFilm2, lblFilm3, lblFilm4, lblFilm5
-            ];
-
-            var client = new MongoClient(passwords.Database);
-            var db = client.GetDatabase("Vilm");
-            _Films = db.GetCollection<FilmInfo>("Films");
-
-            var result = _Films.Aggregate()
-                .Match(x => x.Poster != null)
-                .Sample(5).Project(x => new { x.Poster, x.Title }).ToList();
-
-            for (int i = 0; i < result.Count; i++)
-            {
-                posters[i].Load(result[i].Poster);
-                labels[i].Text = result[i].Title;
-                frmFilmInfoUpdated FilmForm = new(result[i].Title, _LoggedInUser);
-                posters[i].Click += (sender, e) =>
-                {
-                    FilmForm.Show();
-                };
-            }
+            LoadSlideshowPosters();
         }
 
         public frmMainMenu(User user)
         {
             InitializeComponent();
             _LoggedInUser = user;
+
             if (string.IsNullOrWhiteSpace(user.Naam))
             {
                 btnLogin.Text = "Inloggen";
@@ -79,6 +53,35 @@ namespace Film.Kom
             }
 
             DisplayFiveRandomFilmsOnHomePage();
+            LoadSlideshowPosters();
+        }
+
+        private void DisplayFiveRandomFilmsOnHomePage()
+        {
+            List<PictureBox> posters = new() { picFilm1, picFilm2, picFilm3, picFilm4, picFilm5 };
+            List<Label> labels = new() { lblFilm1, lblFilm2, lblFilm3, lblFilm4, lblFilm5 };
+
+            var client = new MongoClient(passwords.Database);
+            var db = client.GetDatabase("Vilm");
+            _Films = db.GetCollection<FilmInfo>("Films");
+
+            var result = _Films.Aggregate()
+                .Match(x => x.Poster != null)
+                .Sample(5)
+                .Project(x => new { x.Poster, x.Title })
+                .ToList();
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                posters[i].Load(result[i].Poster);
+                labels[i].Text = result[i].Title;
+
+                frmFilmInfo FilmForm = new(result[i].Title, _LoggedInUser);
+                posters[i].Click += (sender, e) =>
+                {
+                    FilmForm.Show();
+                };
+            }
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
@@ -100,14 +103,17 @@ namespace Film.Kom
         {
             SearchForFilmsInDB SearchForFilms = new();
             var MovieInfo = await SearchForFilms.SearchFunction(txtSearch.Text.Trim().ToLower());
+
             if (MovieInfo == null)
             {
-                MessageBox.Show($"Film is niet gevonden. probeer het opnieuw");
+                MessageBox.Show($"Film is niet gevonden. Probeer het opnieuw.");
                 return;
             }
+
             MessageBox.Show($"Film {MovieInfo.Title} is gevonden. Hij draait in zaal {MovieInfo.Zaal} om {MovieInfo.Speeltijd}.");
             picFilm1.Load(MovieInfo.Poster);
-            frmFilmInfoUpdated filmForm = new(MovieInfo.Title, _LoggedInUser);
+
+            frmFilmInfo filmForm = new(MovieInfo.Title, _LoggedInUser);
             picFilm1.MouseClick += (sender, e) =>
             {
                 filmForm.Show();
@@ -119,9 +125,46 @@ namespace Film.Kom
             txtSearch.Text = "";
         }
 
-        private void lblFilm5_Click(object sender, EventArgs e)
-        {
+        private void lblFilm5_Click(object sender, EventArgs e) { }
 
+        // --- Slideshow timer tick ---
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (_slideshowPosters.Count == 0) return;
+
+            string nextPoster;
+            do
+            {
+                int index = _random.Next(_slideshowPosters.Count);
+                nextPoster = _slideshowPosters[index];
+            } while (nextPoster == _lastPoster && _slideshowPosters.Count > 1);
+
+            picSlideshow.Load(nextPoster);
+            _lastPoster = nextPoster;
+        }
+
+        // --- Load posters for slideshow ---
+        private void LoadSlideshowPosters()
+        {
+            var client = new MongoClient(passwords.Database);
+            var db = client.GetDatabase("Vilm");
+            var films = db.GetCollection<FilmInfo>("Films");
+
+            _slideshowPosters = films
+                .Find(f => f.Poster != null)
+                .Project(f => f.Poster)
+                .ToList();
+
+            if (_slideshowPosters.Count > 0)
+            {
+                int startIndex = _random.Next(_slideshowPosters.Count);
+                picSlideshow.Load(_slideshowPosters[startIndex]);
+                _lastPoster = _slideshowPosters[startIndex];
+
+                timer1.Interval = 3000; // 3 seconden
+                timer1.Tick += timer1_Tick;
+                timer1.Start();
+            }
         }
     }
 }
